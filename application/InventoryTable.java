8d5d5e6505a0a9a477ed046f9297cd34fb30141c
qye25 <46972449+qye25@users.noreply.github.com>
2019-04-24 19:09:46 -0500

    Add files via upload

--- application/InventoryTable.java
+++ application/InventoryTable.java
@@ -1,160 +1,174 @@
-package application;
+package CS400ATeam.application;
 
-import java.util.ArrayList;
 
-public class InventoryTable<K, V> {
+public class InventoryTable implements HashTableADT {
 
-	private class HashNode<K, V>{
+    private class Node {
+        private String key;
+        private String location;
+        private String date;
+        private Node next;
 
-		K key; 
-		V value; 
+        // Initialize a hashNode with given key and value
+        // Key cannot be null, or else throw IllegalNullKeyException
+        private Node(String key, String location, String date) {
+            if (key != null) {
 
-		 HashNode<K, V> next; 
-		 
-		// Constructor 
-		private HashNode(K key, V value) 
-		{ 
-			this.key = key; 
-			this.value = value; 
-		} 
+                this.key = key;
+                this.location = location;
+                this.next = null;
+            }
+        }
+    }
 
-	}
-	// bucketArray is used to store array of chains 
-    private ArrayList<HashNode<K, V>> bucketArray; 
-  
-    // Current capacity of array list 
-    private int numBuckets; 
-  
-    // Current size of array list 
-    private int size; 
-  
-    // Constructor (Initializes capacity, size and 
-    // empty chains. 
-    public InventoryTable() 
-    { 
-        bucketArray = new ArrayList<>(); 
-        numBuckets = 10; 
-        size = 0; 
-  
-        // Create empty chains 
-        for (int i = 0; i < numBuckets; i++) 
-            bucketArray.add(null); 
-    } 
-  
-    public int size() { return size; } 
-    public boolean isEmpty() { return size() == 0; } 
-  
-    // This implements hash function to find index 
-    // for a key 
-    private int getBucketIndex(K key) 
-    { 
-        int hashCode = key.hashCode(); 
-        int index = hashCode % numBuckets; 
-        return index; 
-    } 
-  
-    // Method to remove a given key 
-    public V remove(K key) 
-    { 
-        // Apply hash function to find index for given key 
-        int bucketIndex = getBucketIndex(key); 
-  
-        // Get head of chain 
-        HashNode<K, V> head = bucketArray.get(bucketIndex); 
-  
-        // Search for key in its chain 
-        HashNode<K, V> prev = null; 
-        while (head != null) 
-        { 
-            // If Key found 
-            if (head.key.equals(key)) 
-                break; 
-  
-            // Else keep moving in chain 
-            prev = head; 
-            head = head.next; 
-        } 
-  
-        // If key was not there 
-        if (head == null) 
-            return null; 
-  
-        // Reduce size 
-        size--; 
-  
-        // Remove key 
-        if (prev != null) 
-            prev.next = head.next; 
-        else
-            bucketArray.set(bucketIndex, head.next); 
-  
-        return head.value; 
-    } 
-  
-    // Returns value for a key 
-    public V get(K key) 
-    { 
-        // Find head of chain for given key 
-        int bucketIndex = getBucketIndex(key); 
-        HashNode<K, V> head = bucketArray.get(bucketIndex); 
-  
-        // Search key in chain 
-        while (head != null) 
-        { 
-            if (head.key.equals(key)) 
-                return head.value; 
-            head = head.next; 
-        } 
-  
-        // If key not found 
-        return null; 
-    } 
-  
-    // Adds a key value pair to hash 
-    public void add(K key, V value) 
-    { 
-        // Find head of chain for given key 
-        int bucketIndex = getBucketIndex(key); 
-        HashNode<K, V> head = bucketArray.get(bucketIndex); 
-  
-        // Check if key is already present 
-        while (head != null) 
-        { 
-            if (head.key.equals(key)) 
-            { 
-                head.value = value; 
-                return; 
-            } 
-            head = head.next; 
-        } 
-  
-        // Insert key in chain 
-        size++; 
-        head = bucketArray.get(bucketIndex); 
-        HashNode<K, V> newNode = new HashNode<K, V>(key, value); 
-        newNode.next = head; 
-        bucketArray.set(bucketIndex, newNode); 
-  
-        // If load factor goes beyond threshold, then 
-        // double hash table size 
-        if ((1.0*size)/numBuckets >= 0.7) 
-        { 
-            ArrayList<HashNode<K, V>> temp = bucketArray; 
-            bucketArray = new ArrayList<>(); 
-            numBuckets = 2 * numBuckets; 
-            size = 0; 
-            for (int i = 0; i < numBuckets; i++) 
-                bucketArray.add(null); 
-  
-            for (HashNode<K, V> headNode : temp) 
-            { 
-                while (headNode != null) 
-                { 
-                    add(headNode.key, headNode.value); 
-                    headNode = headNode.next; 
-                } 
-            } 
-        } 
-    } 
-		
+    private int capacity;
+    private double loadFactorThreshold;
+    private int size;
+    private Node[] table;
+
+    // Constructor
+    InventoryTable() {
+        this(0.5, 11);
+    }
+
+    public InventoryTable(double loadFactorThreshold, int capacity) {
+        this.capacity = capacity;
+        this.loadFactorThreshold = loadFactorThreshold;
+        table = new Node[capacity];
+        size = 0;
+    }
+
+    public int hash(String key) {
+        return (int) Math.abs(key.hashCode() % capacity);
+    }
+
+    @Override
+    public String[] get(String key) {
+        // TODO Auto-generated method stub
+        int index = hash(key);
+        Node cur = table[index];
+        while (cur.next != null) {
+            if (cur.key.equals(key)) {
+                String[] result = {cur.location, cur.date};
+                return result;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+
+    @Override
+    public void add(String key, String location, String date) {
+        // TODO Auto-generated method stub
+        insertHelper(key, location, date, table);
+
+        if (getLoadFactor() > loadFactorThreshold) {// if LF threshold is reached, resize and rehash
+            this.capacity = 2 * capacity + 1;
+            size = 0;
+            Node[] newtable = new Node[capacity];// expand array
+            for (Node node : table) {
+                Node cur = node;
+                while (cur != null) {
+                    insertHelper(cur.key, cur.location, cur.date, newtable);
+                    cur = cur.next;
+                }
+            }
+            this.table = newtable.clone();
+        }
+
+    }
+
+    private void insertHelper(String key, String location, String date, Node[] table) {
+        Node newnode = new Node(key, location, date);
+        if (contains(key)) {
+            return;
+        }
+        if (table[hash(key)] == null) {
+            table[hash(key)] = newnode;
+            size++;
+        } else {
+            Node cur = table[hash(key)];
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = newnode;
+            size++;
+        }
+    }
+
+    private double getLoadFactor() {
+        // TODO Auto-generated method stub
+        return (double) size / capacity;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return size == 0;
+    }
+
+    @Override
+    public int getSize() {
+        // TODO Auto-generated method stub
+        return this.size;
+    }
+
+
+    @Override
+    public boolean remove(String key) {
+        // TODO Auto-generated method stub
+        
+        if (key == null) {
+            return false;
+        } else {
+            int index = hash(key);
+            if (table[index] == null) {
+                // empty linkedlist
+                return false;
+            }
+
+            Node cur = table[index];
+            if (cur.key.equals(key)) {
+                // remove head
+                table[index] = cur.next;
+                size--;
+                return true;
+            }
+            while (cur.next != null) {
+                if (cur.next.key.equals(key) && cur.next.next != null) {
+                    // remove middle
+                    cur.next = cur.next.next;
+                    size--;
+                    return true;
+                } else if (cur.next.key.equals(key) && cur.next.next == null) {
+                    // remove tail
+                    cur.next = null;
+                    size--;
+                    return true;
+                }
+                cur = cur.next;
+            }
+            return false;
+        }
+
+    }
+
+    
+
+    public boolean contains(String key) {
+        int index = hash(key);
+        if (table[index] == null) {
+            return false;
+        } else {
+            Node cur = table[hash(key)];
+            while (cur != null) {
+                if (cur.key.equals(key)) {
+                    return true;
+                }
+                cur = cur.next;
+            }
+        }
+        return false;
+    }
+
 }
